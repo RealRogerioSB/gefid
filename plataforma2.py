efae3231e19cd8dc2722b6ec5f717e40da6cd18f
Rogério Balloussier <143833175+RealRogerioSB@users.noreply.github.com>
2025-03-07 14:59:55 -0300

    Update plataforma2.py

--- plataforma2.py
+++ plataforma2.py
@@ -3,52 +3,53 @@ import streamlit as st
 
 st.set_page_config(
     page_title="Intranet DIOPE GEFID",
-    page_icon="img/bb_ico.ico",
+    page_icon="img/bbfav.svg",
     layout="wide",
 )
 
-st.logo("img/bb_logo.jpg", size="large", link="https://gefid-aplic-1.intranet.bb.com.br/")
+st.logo("img/bb_png.png", size="large", link="https://gefid-aplic-1.intranet.bb.com.br/")
 
-header = st.columns([1.3, 12], vertical_alignment="center")
-header[0].image("img/bb.png", width=100)
-header[1].header("DIOPE GEFID")
-header[1].subheader("Plataforma de Serviços de Escrituração")
+st.header("DIOPE GEFID")
+st.subheader("Diretoria Operações - Gerência de Serviços Fiduciários")
 
-titles = ["Atendimento aos Clientes", "Atendimento aos Investidores", "Obrigações / Rotinas", "Declarações Diversas"]
 cards = [
-    ["Base de Investidores", "Rendimentos Distribuídos", "Rendimentos Pagos", "Rendimentos Pendentes",
-     "DIPJ", "Cálculo de Rendimentos", "EDIV", "Autorregulação BB"],
-    ["Informe de Rendimentos", "Extrato de Movimentação", "Extrato de Rendimentos", "Consulta Cautelas (ABB/BBA)"],
-    ["Circular BACEN 3945", "Circular BACEN 3624", "Resolução CVM 160"],
-    ["Ações em Tesouraria", "Cancelamento de CEPAC", "Maiores Investidores", "Maiores Investidores Percentual"]
+    "Aplicações e Resgates", "BB Escrituração", "Captura CRA - Valores",
+    "Conciliador - Conta Corrente\nControladoria de Fundos", "Conciliador Internacional (2024)",
+    "Conversor EGT", "Grandes Números - Precificação", "Módulo CBIO", "Módulo CBIO - Custódia",
+    "Módulo CBIO - Negociação", "Módulo de Carteiras", "Módulo de Clientes",
+    "Módulo de Cobrança de Tarifas GEFID", "Módulo de Conciliação CETIP-B3 x Drive",
+    "Módulo de Conciliação de Conta\nCaixa de Fundos", "Módulo de Conciliação Drive x GFI",
+    "Módulo de Conciliação em Moeda Estrangeira", "Módulo de Conciliação GFI x CETIP",
+    "Módulo de Conciliação Internacional", "Módulo de Conciliação IPN",
+    "Módulo de Conciliação Selic x SAC", "Módulo de Demonstrações Contábeis de Fundos",
+    "Módulo de Dias Utéis", "Módulo de Integração de Tarifas", "Módulo de Lançamentos a Estornar - Contabilidade",
+    "Módulo de Lançamentos EGT", "Módulo de Precificação", "Módulo de Precificação de CRIs",
+    "Módulo de Usuários", "Módulo DEB-EGT Selic", "Módulo Fundos - Brasilprev",
+    "Módulo Gerador de Relatório Rendimento Cotista", "Módulo Importador de Fundos",
+    "Módulo Impressão GFI", "Módulo Inventário", "Módulo Liquidação Financeira EGT BI-B3",
+    "Módulo Papel Zero", "Módulo Papel Zero - Boletas CETIP", "Módulo Papel Zero - Boletas Selic",
+    "Papel Zero - Liquidação BBDTVM", "Partidas", "Taxa de Administração"
 ]
 
 params = dict(type="primary", use_container_width=True)
 
-acumula = 0
+row, cont = st.columns([3.2, 0.8]), 0
 
-for card in range(len(cards)):
-    row, cont = st.columns([3.2, 0.8]), 0
-    with row[0]:
-        with st.container(border=True):
-            st.write(f"**{titles[card]}**")
-            for _ in range(math.ceil(len(cards[card]) / 4)):
-                for col in st.columns(4):
-                    if cont < len(cards[card]):
-                        with col:
-                            with st.container(height=140, border=True):
-                                st.write(f"**{cards[card][cont]}**")
-                                st.button("**Acessar**", key=f"btn_{acumula}", **params)
-                                cont += 1
-                                acumula += 1
-                    else:
-                        break
+with row[0]:
+    with st.container(border=True):
+        st.write("**Aplicativos**")
+        for _ in range(math.ceil(len(cards) / 4)):
+            for col in st.columns(4):
+                if cont < len(cards):
+                    with col:
+                        with st.container(border=True):
+                            st.button(f"**{cards[cont]}**", key=f"btn_{cont:02d}", **params)
+                    cont += 1
+                else:
+                    break
 
-    with row[1]:
-        if card < 1:
-            with st.container(border=True):
-                st.write("**Navegação**")
-                st.link_button("**Intranet BB**", url="https://intranet.bb.com.br/", **params)
-                st.link_button("**Portal Diope**", url="https://portal.diope.bb.com.br/", **params)
-                st.link_button("**Portal GEFID**", url="https://gefid-aplic-1.intranet.bb.com.br/", **params)
-                st.link_button("**Menu Escrituração**", url="http://localhost:8501", **params)
+with row[1]:
+    with st.container(border=True):
+        st.write("**Navegação**")
+        st.link_button("**Intranet BB**", url="https://intranet.bb.com.br/", **params)
+        st.link_button("**Demandas Externas**", url="https://www.bb.com.br/", **params)
