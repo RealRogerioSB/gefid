aeacd0214a12934d5e39e64da6daf3ad2dbf9ff3
Rogério Balloussier <rogerioballoussier@icloud.com>
2025-04-03 17:12:20 -0300

    updated commit...

--- plataforma.py
+++ plataforma.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from streamlit.navigation.page import StreamlitPage
 
 st.set_page_config(
     page_title="Intranet DIOPE GEFID",
@@ -9,41 +8,44 @@ st.set_page_config(
 
 st.logo(image="img/bb_png.png", size="large", link="https://gefid-aplic-1.intranet.bb.com.br/")
 
-pages: dict[str, list[StreamlitPage]] = {
-    "Home": [
-        st.Page(page="apps/home.py", title="BB Escrituração", icon=":material/home:", default=True),
-    ],
-    "Atendimento aos Clientes": [
-        st.Page(page="apps/base-investidores.py", title="Base de Investidores", icon=":material/account_balance:"),
-        st.Page(page="apps/rendimentos-distribuidos.py", title="Rendimentos Distribuídos", icon=":material/send_money:"),
-        st.Page(page="apps/rendimentos-pagos.py", title="Rendimentos Pagos", icon=":material/paid:"),
-        st.Page(page="apps/rendimentos-pendentes.py", title="Rendimentos Pendentes", icon=":material/savings:"),
-        st.Page(page="apps/dipj.py", title="DIPJ", icon=":material/dynamic_form:"),
-        st.Page(page="apps/calculo-rendimentos.py", title="Cálculo de Rendimentos", icon=":material/calculate:"),
-        st.Page(page="apps/ediv.py", title="EDIV", icon=":material/diversity_3:"),
-        st.Page(page="apps/autorregulacao-bb.py", title="Autorregulação BB", icon=":material/published_with_changes:"),
-    ],
-    "Atendimento aos Investidores": [
-        st.Page(page="apps/informe-rendimentos.py", title="Informe de Rendimentos", icon=":material/ad:"),
-        st.Page(page="apps/extrato-movimentacao.py", title="Extrato de Movimentação", icon=":material/wysiwyg:"),
-        st.Page(page="apps/extrato-rendimentos.py", title="Extrato de Rendimentos", icon=":material/local_atm:"),
-        st.Page(page="apps/consulta-cautelar.py", title="Consulta Cautelas (ABB/BBA)", icon=":material/autorenew:"),
-    ],
-    "Obrigações / Rotinas": [
-        st.Page(page="apps/circular-3945.py", title="Circular BACEN 3945", icon=":material/cycle:"),
-        st.Page(page="apps/circular-3624.py", title="Circular BACEN 3624", icon=":material/cycle:"),
-        st.Page(page="apps/cvm-160.py", title="Resolução CVM 160", icon=":material/siren:"),
-    ],
-    "Declarações Diversas": [
-        st.Page(page="apps/acoes-tesouraria.py", title="Ações em Tesouraria", icon=":material/bar_chart_4_bars:"),
-        st.Page(page="apps/cancela-cepac.py", title="Cancelamento de CEPAC", icon=":material/hide_source:"),
-        st.Page(page="apps/maiores-investidores.py", title="Maiores Investidores", icon=":material/editor_choice:"),
-        st.Page(page="apps/maiores-investidores-percentual.py", title="Maiores Investidores Percentual",
-                icon=":material/workspace_premium:"),
-    ]
-}
-
-st.navigation(pages, expanded=True).run()
+st.navigation(
+    page={
+        "Home": [
+            st.Page(page="apps/home.py", title="BB Escrituração", icon=":material/home:", default=True),
+        ],
+        "Atendimento aos Clientes": [
+            st.Page(page="apps/base-investidores.py", title="Base de Investidores", icon=":material/account_balance:"),
+            st.Page(page="apps/rendimentos-distribuidos.py", title="Rendimentos Distribuídos",
+                    icon=":material/send_money:"),
+            st.Page(page="apps/rendimentos-pagos.py", title="Rendimentos Pagos", icon=":material/paid:"),
+            st.Page(page="apps/rendimentos-pendentes.py", title="Rendimentos Pendentes", icon=":material/savings:"),
+            st.Page(page="apps/dipj.py", title="DIPJ", icon=":material/dynamic_form:"),
+            st.Page(page="apps/calculo-rendimentos.py", title="Cálculo de Rendimentos", icon=":material/calculate:"),
+            st.Page(page="apps/ediv.py", title="EDIV", icon=":material/diversity_3:"),
+            st.Page(page="apps/autorregulacao-bb.py", title="Autorregulação BB",
+                    icon=":material/published_with_changes:"),
+        ],
+        "Atendimento aos Investidores": [
+            st.Page(page="apps/informe-rendimentos.py", title="Informe de Rendimentos", icon=":material/ad:"),
+            st.Page(page="apps/extrato-movimentacao.py", title="Extrato de Movimentação", icon=":material/wysiwyg:"),
+            st.Page(page="apps/extrato-rendimentos.py", title="Extrato de Rendimentos", icon=":material/local_atm:"),
+            st.Page(page="apps/consulta-cautelar.py", title="Consulta Cautelas (ABB/BBA)", icon=":material/autorenew:"),
+        ],
+        "Obrigações / Rotinas": [
+            st.Page(page="apps/circular-3945.py", title="Circular BACEN 3945", icon=":material/cycle:"),
+            st.Page(page="apps/circular-3624.py", title="Circular BACEN 3624", icon=":material/cycle:"),
+            st.Page(page="apps/cvm-160.py", title="Resolução CVM 160", icon=":material/siren:"),
+        ],
+        "Declarações Diversas": [
+            st.Page(page="apps/acoes-tesouraria.py", title="Ações em Tesouraria", icon=":material/bar_chart_4_bars:"),
+            st.Page(page="apps/cancela-cepac.py", title="Cancelamento de CEPAC", icon=":material/hide_source:"),
+            st.Page(page="apps/maiores-investidores.py", title="Maiores Investidores", icon=":material/editor_choice:"),
+            st.Page(page="apps/maiores-investidores-percentual.py", title="Maiores Investidores Percentual",
+                    icon=":material/workspace_premium:"),
+        ]
+    },
+    expanded=True
+).run()
 
 st.sidebar.markdown("[:material/account_balance: Intranet BB](https://intranet.bb.com.br)")
 
@@ -51,11 +53,10 @@ st.sidebar.markdown("[:material/account_balance: Portal Diope](https://portal.di
 
 st.sidebar.markdown("[:material/account_balance: Portal Gefid](https://gefid-aplic-1intranet.bb.com.br)")
 
-markdown: str = """
-<style>
-    [data-testid='stHeader'] {display: none;}
-    #MainMenu {visibility: hidden} footer {visibility: hidden}
-</style>
-"""
-
-st.markdown(markdown, unsafe_allow_html=True)
+st.markdown(
+    body="""<style>
+        [data-testid='stHeader'] {display: none;}
+        #MainMenu {visibility: hidden} footer {visibility: hidden}
+    </style>""",
+    unsafe_allow_html=True
+)
